5cdb9208b68396c1599e18abadaaddc09270b092
Krittika Roy <119665408+krittikaroy0@users.noreply.github.com>
2024-10-02 13:28:28 +0600

    upload catering
    upload catering

---
+++ catering/assets/js/default.js
@@ -0,0 +1,21 @@
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function() {
+      $(this).removeClass("hover");
+    }
+  );
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
